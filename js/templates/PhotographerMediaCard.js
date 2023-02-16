066bad9054552a561d88e70f8e36221a70d3d337
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-02-16 14:07:53 +0100

    amelioration des element sémantique du dom, ajustement attributs

--- js/templates/PhotographerMediaCard.js
+++ js/templates/PhotographerMediaCard.js
@@ -1,85 +1,87 @@
 class PhotographerMediaCard {
     constructor(mediaPhotographer) {
         this._mediaPhotographer = mediaPhotographer
-        this.$wrapper = document.createElement('figure')
-        this.$wrapper.classList.add('photograph-media-card')
+        this.$wrapper = document.querySelector('.photographer_media')
+
     }
 
-
-    createPhotographerMedia() {
-
+    async createPhotographerMedia(){
+        const elementFigure = document.createElement('figure')
+        elementFigure.style.animation = 'anim-modal-open 0.5s ease-in'
         const elementFigcaption = document.createElement('figcaption')
 
         const elementTxt = document.createElement('p')
         elementTxt.textContent = `${this._mediaPhotographer.title}`
+        elementTxt.className = 'titles-images'
 
         const elementSpan = document.createElement('span')
         elementSpan.className = "like-card counter"
         elementSpan.textContent = `${this._mediaPhotographer.likes}`
 
-        const elementLinkLike = document.createElement('a')
-        elementLinkLike.href = "#"
-        elementLinkLike.setAttribute('onclick', 'addTotalLikes(this)')
+        const elementLinkLike = document.createElement('button')
+        addMultipleAttributes(elementLinkLike, {
+            'type': 'button',
+            'aria-label': 'dislikes',
+            'onclick': 'addTotalLikes(this)'
+        })
+        elementLinkLike.className = "like-card"
+        elementLinkLike.style.backgroundColor = 'transparent'
+        elementLinkLike.style.border = 'none'
 
-        const elementI = document.createElement( 'i' )
+
+        const elementI = document.createElement('strong')
         addMultipleAttributes(elementI, {
-            'class' : 'fa fa-heart'
+            'class': 'fa fa-heart'
         })
         elementI.style.color = '#6e6e6e'
 
         let linkMedia;
-
-        if(this._mediaPhotographer.media.attrType === "mp4") {
-            linkMedia = document.createElement('a')
-            linkMedia.href = '#'
-            linkMedia.setAttribute('onclick' , `openModal()`)
-            const elementMedia = document.createElement('video')
-            elementMedia.setAttribute('controls', 'true')
-            addMultipleAttributes(elementMedia, {
-                'style': 'width:300px; height:250px; object-fit:cover;'
+        if (this._mediaPhotographer.media.attrType === "mp4") {
+            linkMedia = document.createElement('video')
+            linkMedia.setAttribute('controls', 'true')
+            addMultipleAttributes(linkMedia, {
+                'height': '250px',
+                'object-fit': 'contain',
+                'class': 'photograph_media_gallery'
             })
             const elementSource = document.createElement('source')
             addMultipleAttributes(elementSource, {
                 'src': this._mediaPhotographer.media.createLink,
                 'type': 'video/mp4',
-                'style': 'width:300px; height:250px;'
+                'target': '_self'
             })
-
-            const elementMediaDownload = document.createElement('a')
-            elementMediaDownload.href = this._mediaPhotographer.media.createLink
-            elementMediaDownload.textContent = "Télécharger mp4"
-
             linkMedia.appendChild(elementSource)
-            linkMedia.appendChild(elementMediaDownload)
-            linkMedia.appendChild(elementMedia)
+
         }
-        else if(this._mediaPhotographer.media.attrType === "jpg") {
+        else if (this._mediaPhotographer.media.attrType === "webp") {
             linkMedia = document.createElement('a')
-            linkMedia.href = '#'
-            linkMedia.setAttribute('onclick' , `openModal()`)
+            linkMedia.href = '#myCarousel'
+            linkMedia.target = "_self"
+            linkMedia.setAttribute('class', 'photograph_media_gallery')
+
             const elementMedia = document.createElement('img')
             addMultipleAttributes(elementMedia, {
-                'src': this._mediaPhotographer.media.createLink,
-                'type': 'image/jpg',
-                'style': 'width:300px; height:250px;'
+                'src': this._mediaPhotographer.media.createLinkLow,
+                'type': 'image/webp',
+                'width': '800px',
+                'height': '600px',
+                'alt': `${this._mediaPhotographer.title}`
             })
             linkMedia.appendChild(elementMedia)
-
         }
         else {
             throw new Error('Le type de média: ' + this._mediaPhotographer.media.attrType + ', n\'est pas accepter!!')
         }
-        linkMedia.setAttribute('tabindex', 0)
+
         elementFigcaption.appendChild(elementTxt)
         elementFigcaption.appendChild(elementSpan)
         elementLinkLike.appendChild(elementI)
         elementFigcaption.appendChild(elementLinkLike)
 
-        this.$wrapper.appendChild(linkMedia)
-        this.$wrapper.appendChild(elementFigcaption)
+        elementFigure.appendChild(linkMedia)
+        elementFigure.appendChild(elementFigcaption)
 
-        return this.$wrapper
+        return this.$wrapper.appendChild(elementFigure)
     }
-
 
 }
