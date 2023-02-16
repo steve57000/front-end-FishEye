6b106bf561fe5c8abd7da22bb070d1b289694222
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-02-16 14:15:16 +0100

    correction indentation

--- js/templates/PhotographerCard.js
+++ js/templates/PhotographerCard.js
@@ -1,65 +1,62 @@
 class PhotographerCard {
-    constructor(photographer) {
-        this._photographer = photographer;
-    }
+  constructor(photographer) {
+    this._photographer = photographer
+  }
 
-    createPhotographerCard() {
+  createPhotographerCard() {
+    const $elementArticle = document.createElement("article")
+    $elementArticle.classList.add("photographer_card")
 
-        const $elementArticle = document.createElement( 'article' )
-        $elementArticle.classList.add('photographer_card')
+    const elementFigure = document.createElement("figure")
+    elementFigure.className = "figure-photograph-home"
 
-        const elementFigure = document.createElement( 'figure')
-        elementFigure.className = 'figure-photograph-home'
+    const elementLink = document.createElement("a")
+    elementLink.target = "_self"
+    addMultipleAttributes(elementLink, {
+      "href": `?id=${this._photographer.id}`,
+      "aria-label": `Voir le profil du photographe`,
+    })
 
-        const elementLink = document.createElement( 'a')
-        addMultipleAttributes(elementLink, {
-            'href': `?id=${this._photographer.id}`,
-            'aria-label': 'Ouvrir la page du photographe',
-            // 'tabindex': "3",
-            'title': `Voir le profil de ${this._photographer.name}`
-        })
+    const elementImage = document.createElement("img")
+    elementImage.src = `${this._photographer.thumbnail}`
+    elementImage.alt = `Portrait du photographe ${this._photographer.name}`
+    addMultipleAttributes(elementImage, {
+      "width": "200",
+      "height": "200"
+    })
 
-        const elementImage = document.createElement( 'img' )
-        elementImage.src = `${this._photographer.thumbnail}`
-        elementImage.alt = `Photo de profil du photographe ${this._photographer.name}`
-        elementImage.className = 'object-position'
-        addMultipleAttributes(elementImage,{
-            'width': '200',
-            'height': '200'
-        })
+    const elementName = document.createElement("h2")
+    elementName.className = "photographer_card_name"
+    elementName.innerHTML = `${this._photographer.name}`
 
-        const elementName = document.createElement('h2')
-        elementName.className = 'photographer_card_name'
-        elementName.innerHTML = `${this._photographer.name}`
+    const elementFigcaption = document.createElement("figcaption")
+    elementFigcaption.className = "photographer_card_info"
 
-        const elementFigcaption = document.createElement('figcaption')
-        elementFigcaption.className = 'photographer_card_info'
-        // elementFigcaption.setAttribute('tabindex', '3')
+    const elementCity = document.createElement("p")
+    elementCity.className = "photographer_card_city"
+    elementCity.innerHTML = `${this._photographer._city}, ${this._photographer._country}`
 
-        const elementCity = document.createElement('p')
-        elementCity.className = 'photographer_card_city'
-        elementCity.innerHTML = `${this._photographer._city}, ${this._photographer._country}`
+    const elementTagline = document.createElement("p")
+    elementTagline.className = "photographer_card_tagline"
+    elementTagline.innerHTML = `${this._photographer._tagline}`
 
-        const elementTagline = document.createElement('p')
-        elementTagline.className = 'photographer_card_tagline'
-        elementTagline.innerHTML = `${this._photographer._tagline}`
+    const elementPrice = document.createElement('p')
+    elementPrice.className = 'photographer_card_price'
+    elementPrice.innerHTML = `${this._photographer._price}\u20AC/Jour `
+    elementPrice.setAttribute('aria-label', `${this._photographer._price}\u20AC par Jour `)
 
-        const elementPrice = document.createElement('p')
-        elementPrice.className = 'photographer_card_price'
-        elementPrice.innerHTML = `${this._photographer._price}\u20AC/Jour `
+    elementLink.appendChild(elementImage)
+    elementLink.appendChild(elementName)
 
-        elementLink.appendChild(elementImage)
-        elementLink.appendChild(elementName)
+    elementFigcaption.appendChild(elementCity)
+    elementFigcaption.appendChild(elementTagline)
+    elementFigcaption.appendChild(elementPrice)
 
-        elementFigcaption.appendChild(elementCity)
-        elementFigcaption.appendChild(elementTagline)
-        elementFigcaption.appendChild(elementPrice)
+    elementFigure.appendChild(elementLink)
+    elementFigure.appendChild(elementFigcaption)
 
-        elementFigure.appendChild(elementLink)
-        elementFigure.appendChild(elementFigcaption)
+    $elementArticle.appendChild(elementFigure)
 
-        $elementArticle.appendChild(elementFigure)
-
-        return $elementArticle
-    }
+    return $elementArticle
+  }
 }
