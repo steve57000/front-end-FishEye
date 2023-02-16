339baa9255ccbadc78b72af4ffb4bcbc4969610b
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-02-16 14:32:02 +0100

    amélioration de la fonction de changement d'image dans la lightbox, et des tailles

--- js/utils/lightbox.js
+++ js/utils/lightbox.js
@@ -1,48 +1,66 @@
 let slideIndex = 1
-showSlides(slideIndex)
+// showSlides(slideIndex)
 
 function getSlides(n) {
-    console.log(slideIndex)
     showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    console.log(slideIndex)
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     let i;
-    if(document.getElementsByClassName("mySlides")) {
-        const slides = document.querySelectorAll(".mySlides");
+    const slides = document.getElementsByClassName("mySlides");
 
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slides[slideIndex-1].style.display = "block";
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slides[slideIndex-1].style.display = "block";
+
 }
+
 
 function openModal() {
     const lightBoxFocus = document.querySelector("#myLightbox")
-    lightBoxFocus.style.display = "block";
-    const  lightBoxFocusableElements = 'button, [tabindex]:not([tabindex="-1"])';
+    lightBoxFocus.style.display = "flex"
+    lightBoxFocus.style.justifyContent = "flex-start"
+    lightBoxFocus.style.alignItems = "center"
+
+    const lightBoxFocusableElements = 'button, [tabindex]:not([tabindex="-1"])';
     addFocusElement(lightBoxFocus, lightBoxFocusableElements)
+
+    const elementDomDisplay = document.querySelectorAll('#photographer_section_profil, #section-medias, .footer-photographer-page')
+    elementDomDisplay.forEach(displayAll => {
+        displayAll.style.display = "none"
+    })
 }
 
 function closeModal() {
     document.getElementById("myLightbox").style.display = "none";
+    const elementDomDisplay = document.querySelectorAll('#photographer_section_profil, #section-medias, .footer-photographer-page')
+
+    elementDomDisplay[0].style.display = 'block'
+    elementDomDisplay[1].style.display = 'grid'
+    elementDomDisplay[2].style.display = 'flex'
 }
 
-function getMediaLightbox(data) {
+function lightboxBtnKeyDown(event){
 
-    data.forEach(mediaPhotographer => {
-        console.log(mediaPhotographer)
-        const Template = new PhotographerMediaCard(mediaPhotographer)
-        this.$main.appendChild(
-            Template.createPhotographerMedia()
-        )
-    })
+    switch(event.code){
+        case 'ArrowUp':
+        case 'ArrowRight':
+            event.preventDefault()
+            return getSlides(1)
+
+        case 'ArrowDown':
+        case 'ArrowLeft':
+            event.preventDefault()
+            return getSlides(-1)
+
+        default :
+            return
+    }
 }
