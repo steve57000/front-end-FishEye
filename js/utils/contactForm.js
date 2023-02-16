4dabf313599e3ef18a8b82be78b3c7633a799d4b
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-02-16 14:40:19 +0100

    ajout d'attribut aria

--- js/utils/contactForm.js
+++ js/utils/contactForm.js
@@ -1,23 +1,28 @@
 function displayModal() {
-
+    putHiddenDomElement('in')
     const modal = document.getElementById('contact_modal');
-    console.log(modal)
     modal.style.display = 'block'
-    modal.setAttribute('aria-modal', 'true')
-    if(modal.getAttribute('aria-hidden')) {
-        modal.removeAttribute('aria-hidden')
-    }
+    addMultipleAttributes(modal, {
+        'aria-modal': 'true',
+        'aria-expanded': 'true',
+        'aria-hidden': 'false'
+    })
 
-    modal.classList.add('show')
 
     const  selectFocusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
     addFocusElement(modal, selectFocusableElements)
 }
 
 function closeFormModal() {
-
-    document.getElementById('contact_modal').style.display = 'none'
-    const buttonFocus = document.querySelector('.contact_button')
+    putHiddenDomElement('out')
+    const modal = document.getElementById('contact_modal');
+    modal.style.display = 'none'
+    addMultipleAttributes(modal, {
+        'aria-modal': 'false',
+        'aria-expanded': 'false',
+        'aria-hidden': 'true'
+    })
+    const buttonFocus = document.querySelector('#contact_button')
     buttonFocus.focus()
 }
 
@@ -26,3 +31,18 @@ function closeContactConfirm() {
     window.location.reload();
 }
 
+function putHiddenDomElement(type) {
+    const typeEl = type
+    const elementNodeHidden = document.querySelectorAll('#header, #photographer_section_profil, #section-medias,  #myLightbox, .footer-photographer-page')
+    elementNodeHidden.forEach(elementDom => {
+        if(typeEl === 'in'){
+            elementDom.setAttribute('aria-hidden', 'true')
+        }
+        else if(typeEl === 'out'){
+            elementDom.removeAttribute('aria-hidden')
+        }
+        else{
+            throw new Error("Le type d'ouverture et de fermeture de la modal doit être défini sur 'in' ou 'out' Le type trouvé est: '" + typeEl + "'")
+        }
+    })
+}
